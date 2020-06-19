4df717a6d35c83b50fb01031112180696275fd4e
ajay-1608 <62495226+ajay-1608@users.noreply.github.com>
2020-06-19 11:16:20 +0530

    python test

--- client.py
+++ client.py
@@ -53,38 +53,39 @@
 # CLIENT_socket.close()
 # # close the CLIENT socket
 # s.close()
-import logging
-import asyncio
-from rpcudp.protocol import RPCProtocol
+# import logging
+# import asyncio
+# from rpcudp.protocol import RPCProtocol
 
 
-async def sayhi(protocol, address):
-    # call rpc that returns immediately
-    result = await protocol.sayhi_quickly(address, "Fast Snake Plissken")
-    print(result[1] if result[0] else "No response received.")
+# async def sayhi(protocol, address):
+#     # call rpc that returns immediately
+#     result = await protocol.sayhi_quickly(address, "Fast Snake Plissken")
+#     print(result[1] if result[0] else "No response received.")
 
-    # call rpc that delays for a bit
-    result = await protocol.sayhi_slowly(address, "Slow Snake Plissken")
-    print(result[1] if result[0] else "No response received.")
+#     # call rpc that delays for a bit
+#     result = await protocol.sayhi_slowly(address, "Slow Snake Plissken")
+#     print(result[1] if result[0] else "No response received.")
 
 
-logging.basicConfig(level=logging.DEBUG)
-loop = asyncio.get_event_loop()
-loop.set_debug(True)
+# logging.basicConfig(level=logging.DEBUG)
+# loop = asyncio.get_event_loop()
+# loop.set_debug(True)
 
-# Start local UDP server to be able to handle responses
-listen = loop.create_datagram_endpoint(RPCProtocol, local_addr=('0.0.0.0', 4567))
-transport, protocol = loop.run_until_complete(listen)
+# # Start local UDP server to be able to handle responses
+# listen = loop.create_datagram_endpoint(RPCProtocol, local_addr=('0.0.0.0', 4567))
+# transport, protocol = loop.run_until_complete(listen)
 
-# Call remote UDP server to say hi
-func = sayhi(protocol, ('192.168.0.102', 1234))
-loop.run_until_complete(func)
+# # Call remote UDP server to say hi
+# func = sayhi(protocol, ('192.168.0.102', 1234))
+# loop.run_until_complete(func)
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+# try:
+#     loop.run_forever()
+# except KeyboardInterrupt:
+#     pass
 
-transport.close()
-loop.close()
+# transport.close()
+# loop.close()
 #hello world hahahaha
+print ("hello from client")
